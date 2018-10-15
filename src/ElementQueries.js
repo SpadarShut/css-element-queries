61778a85a7a5676c9054ab21d4a0f0a98fcda67d
Spadar Shut <sp.shut@gmail.com>
2018-10-15 12:03:31 +0300

    fix ignoring queries from @import -ed same domain styles

--- src/ElementQueries.js
+++ src/ElementQueries.js
@@ -384,7 +384,7 @@
                     } else if (4 === rules[i].type) {
                         readRules(rules[i].cssRules || rules[i].rules);
                     } else if (3 === rules[i].type) {
-                        if(rules[i].styleSheet.hasOwnProperty("cssRules")) {
+                        if(rules[i].styleSheet.cssRules) {
                             readRules(rules[i].styleSheet.cssRules);
                         }
                     }
